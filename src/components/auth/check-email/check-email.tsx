3b80c7e5e23be0500ad639bdc609f75eab4b7d2d
Kattesh <kate.sh@icloud.com>
2023-07-03 18:39:14 -0500

    feat: add check-email feature

---
+++ src/components/auth/check-email/check-email.tsx
@@ -0,0 +1,28 @@
+import Email from '../../../assets/icons/email.tsx'
+import { Button } from '../../ui/button'
+import { Card } from '../../ui/card'
+import { Typography } from '../../ui/typography'
+
+import s from './check-email.module.scss'
+
+type Props = {
+  email: string
+}
+export const CheckEmail = ({ email }: Props) => {
+  return (
+    <Card className={s.card}>
+      <Typography variant="large" className={s.title}>
+        Check your email
+      </Typography>
+      <div className={s.icon}>
+        <Email />
+      </div>
+      <Typography variant="body2" className={s.instructions}>
+        We have sent an Email with instructions to {email}
+      </Typography>
+      <Button as={'a'} href={'/sing-in'} className={s.signInLink}>
+        Back to Sign in
+      </Button>
+    </Card>
+  )
+}
