560c4822df8e1b0547eb2057cabdfdc419aff688
zanzipanziby <zanzipanziby@gmail.com>
2023-07-05 19:49:17 +0300

    feat: add header component

---
+++ src/components/ui/header/header.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Header } from './header.tsx'
+
+const meta = {
+  title: 'Components/Header',
+  component: Header,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Header>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    name: 'Zakk Wylde',
+    isLoggedIn: true,
+  },
+}
---
+++ src/components/ui/header/header.tsx
@@ -0,0 +1,42 @@
+import avatar from '../../../assets/avatar-test/Avatar-test.svg'
+import Logo from '../../../assets/icons/logo.tsx'
+import { Avatar } from '../avatar'
+import { Button } from '../button'
+import { ContentContainer } from '../content-container'
+import { Typography } from '../typography'
+
+import s from './header.module.scss'
+
+type HeaderPropsType = {
+  isLoggedIn: boolean
+  name?: string
+  avatarSrc?: string
+}
+
+export const Header = ({
+  isLoggedIn = true,
+  name = 'Ivan',
+  avatarSrc = avatar,
+}: HeaderPropsType) => {
+  const loginOrAvatarContent = !isLoggedIn ? (
+    <Button>Sign In</Button>
+  ) : (
+    <div className={s.avatarContainer}>
+      <Typography className={s.name} variant={'subtitle1'}>
+        {name}
+      </Typography>
+      <Avatar src={avatarSrc} />
+    </div>
+  )
+
+  return (
+    <div className={s.header}>
+      <ContentContainer>
+        <div className={s.headerContainer}>
+          <Logo />
+          {loginOrAvatarContent}
+        </div>
+      </ContentContainer>
+    </div>
+  )
+}
