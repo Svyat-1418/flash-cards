6ff4abb177cf396dc347ba76c7a3b4420c8d556b
Kattesh <kate.sh@icloud.com>
2023-07-07 13:10:45 -0500

    fix: slider: add some styles

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,10 +1,9 @@
-import { FC } from 'react'
-
 import * as Slider from '@radix-ui/react-slider'
 
 import { Typography } from '../typography'
 
 import s from './slider.module.scss'
+
 type SliderPropsType = {
   max: number
   min: number
@@ -12,33 +11,37 @@ type SliderPropsType = {
   rangeValue: number[]
   disabled?: boolean
 }
-const changeValueHandler = () => {}
 
-export const SliderRange: FC<SliderPropsType> = ({ min, max, step, rangeValue }) => (
-  <div className={s.container}>
-    <div>
-      <Typography variant={'body1'} className={s.value}>
-        {rangeValue[0]}
-      </Typography>
-    </div>
+export const SliderRange = ({ min, max, step, rangeValue }: SliderPropsType) => {
+  const handleChange = () => {}
 
-    <Slider.Root
-      className={s.root}
-      defaultValue={rangeValue}
-      max={max}
-      min={min}
-      step={step}
-      onChange={changeValueHandler}
-    >
-      <Slider.Track className={s.track}>
-        <Slider.Range className={s.range} />
-      </Slider.Track>
-      <Slider.Thumb className={s.thumb} aria-label="range" />
-    </Slider.Root>
-    <div>
-      <Typography variant={'body1'} className={s.value}>
-        {rangeValue[1]}
-      </Typography>
+  return (
+    <div className={s.container}>
+      <div>
+        <Typography variant={'body1'} className={s.value}>
+          {rangeValue[0]}
+        </Typography>
+      </div>
+
+      <Slider.Root
+        className={s.root}
+        defaultValue={rangeValue}
+        max={max}
+        min={min}
+        step={step}
+        onChange={handleChange}
+      >
+        <Slider.Track className={s.track}>
+          <Slider.Range className={s.range} />
+        </Slider.Track>
+        <Slider.Thumb className={s.thumb} aria-label="range1" />
+        <Slider.Thumb className={s.thumb} aria-label="range2" />
+      </Slider.Root>
+      <div>
+        <Typography variant={'body1'} className={s.value}>
+          {rangeValue[1]}
+        </Typography>
+      </div>
     </div>
-  </div>
-)
+  )
+}
