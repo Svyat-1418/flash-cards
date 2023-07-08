50e69251bde09c8126e183e20864e3a39469be9b
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-07-08 21:07:32 +0300

    feat: add github ci

---
+++ src/components/ui/table/text-field.stories.tsx
@@ -0,0 +1,19 @@
+// import type { Meta, StoryObj } from '@storybook/react'
+//
+// import { Button } from '../button'
+//
+// import { Table } from './table.tsx'
+//
+// const meta = {
+//   title: 'Components/Table',
+//   component: Button,
+//   tags: ['autodocs'],
+// } satisfies Meta<typeof TextField>
+//
+// export default meta
+// type Story = StoryObj<typeof meta>
+//
+// export const Default: Story = {
+//   args: {},
+// }
+export default {}
