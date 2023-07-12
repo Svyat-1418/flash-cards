e6a38a2d58be3b93064c1a38be9468255ab83162
zanzipanziby <zanzipanziby@gmail.com>
2023-07-12 20:20:56 +0300

    feat: add select component and add and update dependencies

---
+++ src/components/ui/select/select.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SelectComponent } from './select.tsx'
+
+const meta = {
+  title: 'Components/SelectComponent',
+  component: SelectComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    optionTextVariant: {
+      options: [
+        'large',
+        'h1',
+        'h2',
+        'h3',
+        'body1',
+        'body2',
+        'subtitle1',
+        'subtitle2',
+        'caption',
+        'overline',
+        'link1',
+        'link2',
+        'error',
+      ],
+      control: { type: 'radio' },
+    },
+  },
+} satisfies Meta<typeof SelectComponent>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
