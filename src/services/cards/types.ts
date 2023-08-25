27ce89dec7fb1642700fc230dfd0982507b0de5a
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-08-25 19:17:39 +0300

    feat: add types for cards-endpoints

---
+++ src/services/cards/types.ts
@@ -0,0 +1,68 @@
+import { Nullable } from '../../types/common.types.ts'
+
+export type GetCardsParams = {
+  deckId: string
+  currentPage?: number
+  itemsPerPage?: number
+  pageSize?: number
+  orderBy?: string | null
+}
+export type GetCardsResponse = {
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  shots: number
+  cover: string
+  rating?: number
+  isDeleted: any
+  isBlocked: Nullable<boolean>
+  created: string
+  updated: string
+  cardsCount: number
+}
+
+export type CreateCardArgs = {
+  question: string
+  answer: string
+  questionImg?: string
+  answerImg?: string
+  questionVideo?: string
+  answerVideo?: string
+}
+export type CreateCardResponse = {
+  id: string
+  deckId: string
+  userId: string
+  question: string
+  answer: string
+  shots: number
+  questionImg?: Nullable<string>
+  answerImg?: Nullable<string>
+  answerVideo?: any
+  questionVideo?: any
+  comments?: any
+  type?: Nullable<string>
+  rating: number
+  moreId?: Nullable<string>
+  created: string
+  updated: string
+}
+
+export type Card = {
+  id: string
+  question: string
+  answer: string
+  deckId: string
+  questionImg?: any
+  answerImg?: any
+  questionVideo?: any
+  answerVideo?: any
+  created: string
+  updated: string
+  shots: number
+  grade: number
+  userId: string
+}
+
+export type DeleteCardParams = { id: string }
