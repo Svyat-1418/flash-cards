fc0f6bba9a60fc29c9eb94dbc9f46a14209c9596
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-08-25 19:27:31 +0300

    feat: add getCards endpoint

--- src/services/cards/cards-endpoints.ts
+++ src/services/cards/cards-endpoints.ts
@@ -1,7 +1,22 @@
 import { isEmpty } from 'remeda'
 
 import { baseApi } from '../base-api.ts'
+import { PaginatedResponse } from '../common.types.ts'
 
-export const cardsEndpoints = baseApi.injectEndpoints({
-  endpoints: builder => ({}),
+import { Card, GetCardsParams } from './types.ts'
+
+const cardsEndpoints = baseApi.injectEndpoints({
+  endpoints: builder => ({
+    getCards: builder.query<PaginatedResponse<Card>, GetCardsParams>({
+      query: ({ deckId, ...params }) => {
+        return {
+          url: `decks/${deckId}/cards`,
+          params: isEmpty(params) ? undefined : params,
+        }
+      },
+      providesTags: ['Cards'],
+    }),
+  }),
 })
+
+export const { useGetCardsQuery } = cardsEndpoints
