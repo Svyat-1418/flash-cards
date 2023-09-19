ed3cc1d2f58fdc84a7cdee00943870ae5ae84f79
zanzipanziby <zanzipanziby@gmail.com>
2023-09-19 18:37:41 +0300

    feat: implemented reset password flow

--- src/utils/recover-password.util.ts
+++ src/utils/recover-password.util.ts
@@ -1,8 +1,6 @@
 import { ForgotPasswordArgs } from '../services/auth/types.ts'
 
 export const recoverPasswordUtil = (email: string): ForgotPasswordArgs => {
-  console.log(email)
-
   return {
     html: "<h1>Hi, ##name##</h1><p>Click <a href='http://localhost:5173/new-password/##token##'>here</a> to recover your password</p>",
     email,
