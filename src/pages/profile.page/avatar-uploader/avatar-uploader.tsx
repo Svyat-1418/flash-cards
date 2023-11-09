98e00ae3587bef003ab8187595ab5ebdb95978cc
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-11-10 01:34:24 +0300

    feat: add avatar uploader component

---
+++ src/pages/profile.page/avatar-uploader/avatar-uploader.tsx
@@ -0,0 +1,40 @@
+import { clsx } from 'clsx'
+
+import { Edit } from '../../../assets/icons/edit'
+import { Avatar } from '../../../components/ui/avatar-with-fallback'
+import { Button } from '../../../components/ui/button'
+import { Uploader } from '../../../components/ui/uploader'
+import { UpdateMeArgs } from '../../../services/auth/types.ts'
+
+import s from './avatar-uploader.module.scss'
+
+type Props = {
+  avatar?: string
+  name: string
+  isEditable?: boolean
+  className?: string
+  handleLoadImage: (data: Pick<UpdateMeArgs, 'name' | 'avatar'>) => void
+}
+
+export const AvatarUploader = ({
+  avatar,
+  name,
+  isEditable = true,
+  handleLoadImage,
+  className,
+}: Props): JSX.Element => {
+  const avatarUploaderClasses = clsx(s.root, className)
+
+  return (
+    <div className={avatarUploaderClasses}>
+      <Avatar image={avatar} userName={name} size="large" />
+      {isEditable && (
+        <Uploader className={s.uploader} onLoadCover={handleLoadImage} onLoadError={() => {}}>
+          <Button>
+            <Edit />
+          </Button>
+        </Uploader>
+      )}
+    </div>
+  )
+}
