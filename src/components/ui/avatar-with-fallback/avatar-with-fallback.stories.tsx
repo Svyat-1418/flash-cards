8bbe71a014970ad30a82a2ccc6fb3afb00a3c4e1
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-11-10 01:03:39 +0300

    feat: add avatar component using @radix

---
+++ src/components/ui/avatar-with-fallback/avatar-with-fallback.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar } from './avatar-with-fallback.tsx'
+
+const meta: Meta<typeof Avatar> = {
+  title: 'Components/AvatarWithFalllback',
+  component: Avatar,
+  tags: ['autodocs'],
+  argTypes: {
+    userName: String,
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    userName: 'John Doe',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    userName: 'John Doe',
+    size: 'large',
+  },
+}
+
+export const DefaultWithImage: Story = {
+  args: {
+    userName: 'John Doe',
+    image: 'https://placehold.co/36',
+  },
+}
+
+export const LargeWithImage: Story = {
+  args: {
+    userName: 'John Doe',
+    size: 'large',
+    image: 'https://placehold.co/96',
+  },
+}
