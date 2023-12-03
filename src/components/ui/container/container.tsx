04a82aaae5d2def2e27e1129dcd5cf3c9fe1d198
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-12-03 15:55:45 +0300

    fix: rename variable

--- src/components/ui/container/container.tsx
+++ src/components/ui/container/container.tsx
@@ -6,8 +6,8 @@ import s from './container.module.scss'
 
 export const Container = forwardRef<ElementRef<'div'>, ComponentPropsWithoutRef<'div'>>(
   ({ className, ...restProps }, ref): JSX.Element => {
-    const pageClasses = clsx(s.root, className)
+    const containerClasses = clsx(s.root, className)
 
-    return <div ref={ref} className={pageClasses} {...restProps} />
+    return <div ref={ref} className={containerClasses} {...restProps} />
   }
 )
