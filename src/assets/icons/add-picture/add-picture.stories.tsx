35b51efc5ea1b84355d8b1a7a98b6c5f6709ab63
zanzipanziby <zanzipanziby@gmail.com>
2023-12-04 00:49:59 +0300

    feat: add icon add-picture

---
+++ src/assets/icons/add-picture/add-picture.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import AddPicture from './add-picture.tsx'
+
+const meta = {
+  title: 'Icons/Add Picture',
+  component: AddPicture,
+} satisfies Meta<typeof AddPicture>
+
+type Story = StoryObj<typeof meta>
+export default meta
+
+export const Default: Story = {
+  args: {},
+}
