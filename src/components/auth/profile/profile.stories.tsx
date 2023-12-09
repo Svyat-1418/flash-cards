bae4ae8ddfc3acef1fb1d2de9f62c998374e4dcc
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-12-09 17:12:26 +0300

    fix: temporary changes

--- src/components/auth/profile/profile.stories.tsx
+++ src/components/auth/profile/profile.stories.tsx
@@ -11,4 +11,12 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    avatarSrc: '',
+    name: '',
+    email: '',
+    logout: () => {},
+    handleUpdateMe: () => {},
+  },
+}
