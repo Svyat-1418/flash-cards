1e6f3f5c38a9508af6b4cf07dd0924dd2b350022
zanzipanziby <zanzipanziby@gmail.com>
2023-12-11 03:03:07 +0300

    feat: added skeleton-table component

---
+++ src/components/ui/skeleton/skeleton-table.tsx
@@ -0,0 +1,158 @@
+import ContentLoader from 'react-content-loader'
+
+export const SkeletonTable = () => (
+  <ContentLoader
+    speed={2}
+    width={1000}
+    height={550}
+    viewBox="0 0 1000 550"
+    backgroundColor="#A0A0A0"
+    foregroundColor="#ffffff"
+  >
+    {/*<rect x="0" y="45" rx="3" ry="3" width="906" height="17" />*/}
+    {/*<circle cx="879" cy="123" r="11" />*/}
+    {/*<circle cx="914" cy="123" r="11" />*/}
+    {/*<rect x="0" y="115" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="0" y="114" rx="3" ry="3" width="299" height="15" />*/}
+    {/*<rect x="0" y="114" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="0" y="155" rx="3" ry="3" width="897" height="2" />*/}
+    {/*<circle cx="880" cy="184" r="11" />*/}
+    {/*<circle cx="915" cy="184" r="11" />*/}
+    {/*<rect x="105" y="176" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="306" y="175" rx="3" ry="3" width="299" height="15" />*/}
+    {/*<rect x="662" y="175" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="56" y="216" rx="3" ry="3" width="897" height="2" />*/}
+    {/*<circle cx="881" cy="242" r="11" />*/}
+    {/*<circle cx="916" cy="242" r="11" />*/}
+    {/*<rect x="106" y="234" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="307" y="233" rx="3" ry="3" width="299" height="15" />*/}
+    {/*<rect x="663" y="233" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="57" y="274" rx="3" ry="3" width="897" height="2" />*/}
+    {/*<circle cx="882" cy="303" r="11" />*/}
+    {/*<circle cx="917" cy="303" r="11" />*/}
+    {/*<rect x="107" y="295" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="308" y="294" rx="3" ry="3" width="299" height="15" />*/}
+    {/*<rect x="664" y="294" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="58" y="335" rx="3" ry="3" width="897" height="2" />*/}
+    {/*<circle cx="881" cy="363" r="11" />*/}
+    {/*<circle cx="916" cy="363" r="11" />*/}
+    {/*<rect x="106" y="355" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="307" y="354" rx="3" ry="3" width="299" height="15" />*/}
+    {/*<rect x="663" y="354" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="57" y="395" rx="3" ry="3" width="897" height="2" />*/}
+    {/*<circle cx="882" cy="424" r="11" />*/}
+    {/*<circle cx="917" cy="424" r="11" />*/}
+    {/*<rect x="107" y="416" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="308" y="415" rx="3" ry="3" width="299" height="15" />*/}
+    {/*<rect x="664" y="415" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="55" y="453" rx="3" ry="3" width="897" height="2" />*/}
+    {/*<rect x="51" y="49" rx="3" ry="3" width="2" height="465" />*/}
+    {/*<rect x="955" y="49" rx="3" ry="3" width="2" height="465" />*/}
+    {/*<circle cx="882" cy="484" r="11" />*/}
+    {/*<circle cx="917" cy="484" r="11" />*/}
+    {/*<rect x="107" y="476" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="308" y="475" rx="3" ry="3" width="299" height="15" />*/}
+    {/*<rect x="664" y="475" rx="3" ry="3" width="141" height="15" />*/}
+    {/*<rect x="52" y="513" rx="3" ry="3" width="904" height="2" />*/}
+    {/*<rect x="52" y="80" rx="3" ry="3" width="905" height="17" />*/}
+    {/*<rect x="53" y="57" rx="3" ry="3" width="68" height="33" />*/}
+    {/*<rect x="222" y="54" rx="3" ry="3" width="149" height="33" />*/}
+    {/*<rect x="544" y="55" rx="3" ry="3" width="137" height="33" />*/}
+    {/*<rect x="782" y="56" rx="3" ry="3" width="72" height="33" />*/}
+    {/*<rect x="933" y="54" rx="3" ry="3" width="24" height="33" />*/}
+    <rect x={0} y={0} rx={3} ry={3} width={980} height={50} />
+    {/*наполнение*/}
+    <rect x={15} y={70} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={70} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={70} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={70} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="75" r="10" />
+    <circle cx="930" cy="75" r="10" />
+    <circle cx="960" cy="75" r="10" />
+    {/*end*/}
+    <rect x={0} y={100} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={120} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={120} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={120} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={120} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="125" r="10" />
+    <circle cx="930" cy="125" r="10" />
+    <circle cx="960" cy="125" r="10" />
+    {/*end 2*/}
+    <rect x={0} y={150} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={170} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={170} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={170} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={170} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="175" r="10" />
+    <circle cx="930" cy="175" r="10" />
+    <circle cx="960" cy="175" r="10" />
+    {/*end*/}
+    <rect x={0} y={200} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={220} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={220} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={220} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={220} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="225" r="10" />
+    <circle cx="930" cy="225" r="10" />
+    <circle cx="960" cy="225" r="10" />
+    {/*end*/}
+    <rect x={0} y={250} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={270} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={270} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={270} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={270} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="275" r="10" />
+    <circle cx="930" cy="275" r="10" />
+    <circle cx="960" cy="275" r="10" />
+    {/*end*/}
+    <rect x={0} y={300} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={320} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={320} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={320} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={320} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="325" r="10" />
+    <circle cx="930" cy="325" r="10" />
+    <circle cx="960" cy="325" r="10" />
+    {/*end*/}
+    <rect x={0} y={350} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={370} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={370} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={370} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={370} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="375" r="10" />
+    <circle cx="930" cy="375" r="10" />
+    <circle cx="960" cy="375" r="10" />
+    {/*end*/}
+    <rect x={0} y={400} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={420} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={420} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={420} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={420} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="425" r="10" />
+    <circle cx="930" cy="425" r="10" />
+    <circle cx="960" cy="425" r="10" />
+    {/*end*/}
+    <rect x={0} y={450} rx={3} ry={3} width={980} height={1} />
+    {/*наполнение*/}
+    <rect x={15} y={470} rx={3} ry={3} width={250} height={10} />
+    <rect x={350} y={470} rx={3} ry={3} width={50} height={10} />
+    <rect x={500} y={470} rx={3} ry={3} width={100} height={10} />
+    <rect x={700} y={470} rx={3} ry={3} width={100} height={10} />
+    <circle cx="900" cy="475" r="10" />
+    <circle cx="930" cy="475" r="10" />
+    <circle cx="960" cy="475" r="10" />
+    {/*end*/}
+    <rect x={0} y={500} rx={3} ry={3} width={980} height={1} />
+    {/*вертикаль*/}
+    <rect x={0} y={0} rx={3} ry={3} width={1} height={500} />
+    <rect x={980} y={0} rx={3} ry={3} width={1} height={500} />
+  </ContentLoader>
+)
